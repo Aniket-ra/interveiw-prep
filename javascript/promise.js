84c12f1fc183bdec1b407dd720813c9e16b76661
Aniket raj <ar18.raj@gmail.com>
2023-02-07 11:20:46 +0530

    first commit

---
+++ javascript/promise.js
@@ -0,0 +1,25 @@
+//promises in javascript
+// callbacks
+console.log("start")
+// function action(username, cb){
+//     setTimeout(() =>{
+//         cb(`Subscribe to ${username}`)
+//     },1000);
+// }
+// const mess=action("my channel", function(mess){
+//     console.log(mess)
+// })
+// const sub=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         const result=true;
+//         if(result) resolve("your are promoted");
+//         else reject(new Error("not qualified"))
+
+//     },2000)
+// })
+// sub.then((res)=>{
+// console.log(res)
+// }).catch((err)=>{
+// console.log(err)
+// })
+// console.log("stop");
